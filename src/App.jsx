51ae41375b15d81e6c0909e9e63203a0b0e03956
Schlottik <Schlottag31@outlook.com>
2024-10-15 16:26:11 -0400

    batman

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import CalendarApp from "./components/CalendarApp"
+import './components/CalendarApp.css'
+const App = () => {
+  return (
+    <div className="container">
+      <CalendarApp />
+    </div>
+  )
+}
+
+export default App
